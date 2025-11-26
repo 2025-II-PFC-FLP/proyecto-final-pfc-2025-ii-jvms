626c40b8110f66e14c3ece88a5b8951203ba5805
VictorMuri06 <victor.goyes@correounivalle.du.co>
2025-11-25 19:05:35 -0500

    CostoRiego y CostoMovilidad

--- app/src/main/scala/taller/RiegoCostos.scala
+++ app/src/main/scala/taller/RiegoCostos.scala
@@ -1,5 +1,34 @@
 package taller
 
+import RiegoBase._
+
 class RiegoCostos {
+
+  // 1. costoRiegoFinca
+  // Suma el costo de todos los tablones usando foldLeft
+  def costoRiegoFinca(f: Finca, order: ProgRiego): Int = {
+    val indices = (0 until f.length).toVector
+
+    indices.foldLeft(0) { (acc, i) =>
+      acc + costoRiegoTablon(i, f, order)
+    }
+  }
+
+
+  // 2. costoMovilidad
+  // Suma DF[π(j)][π(j+1)]
+  def costoMovilidad(f: Finca, order: ProgRiego, d: Distancia): Int = {
+
+    def recorrer(j: Int): Int = {
+      if (j >= order.length - 1) 0
+      else {
+        val actual = order(j)
+        val siguiente = order(j + 1)
+        d(actual)(siguiente) + recorrer(j + 1)
+      }
+    }
+
+    recorrer(0)
+  }
 
 }
